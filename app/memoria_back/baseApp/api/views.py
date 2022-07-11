6dfbe2eaea3e6d46fb4108673743f16d1885c610
Bastian Sepulveda <bastian.sepulvedagarcia@gmail.com>
2022-07-10 23:25:58 -0400

    Implementacion drf-yasg para documentacion API
    Implementacion drf-yasg 
    creacion ruta /docs 
    creacion ruta /redocs

--- app/memoria_back/baseApp/api/views.py
+++ app/memoria_back/baseApp/api/views.py
@@ -1,12 +1,21 @@
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.http import Http404
 
 from baseApp.models import LocalComercial
 from baseApp.api.serializers import LocalComercialSerializer_get, LocalComercialSerializer_post
 
 
 class LocalComercialApiView(APIView):
+    def get_object(self, pk):
+        try:
+            return LocalComercial.objects.get(pk=pk)
+        except LocalComercial.DoesNotExist:
+            raise Http404
+    """
+    List all LocalComercial, or create a new LocalComercial.
+    """
     def get(self, request):
         serializer = LocalComercialSerializer_get(LocalComercial.objects.all(), many=True)
         return Response(status=status.HTTP_200_OK, data=serializer.data)
@@ -16,3 +25,13 @@ class LocalComercialApiView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(status=status.HTTP_201_CREATED,data=serializer.data)
+    """
+    Retrieve, update or delete a LocalComercial instance.
+    """
+    def get(self, request, format=None):
+        locales = LocalComercial.objects.all()
+        serializer = LocalComercialSerializer_get(locales, many=True)
+        return Response(serializer.data)
+
+
+    
--- app/memoria_back/memoria_back/urls.py
+++ app/memoria_back/memoria_back/urls.py
@@ -17,7 +17,28 @@ from baseApp.api.views import LocalComercialApiView
 from django.contrib import admin
 from django.urls import path
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="MYSYSTEM API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/localComercials/',LocalComercialApiView.as_view())
+    path('api/localComercials/',LocalComercialApiView.as_view()),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redocs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
 ]
