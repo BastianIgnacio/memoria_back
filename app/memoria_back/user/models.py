5e3fa20bd4c9c7247004bc0997aba4f08b4d3ee7
Bastian Sepulveda <bastian.sepulvedagarcia@gmail.com>
2022-07-18 20:20:42 -0400

    Modificación USERNAME_FIELD por email

--- app/memoria_back/user/models.py
+++ app/memoria_back/user/models.py
@@ -3,9 +3,11 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
+    email = models.EmailField(unique=True)
     nombre = models.CharField(max_length=40, blank=True)
     apellido = models.CharField(max_length=40, blank= True)
     telefono = models.CharField(max_length=15,blank= True)
     rol = models.CharField(max_length=20, blank=True)
 
-
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
